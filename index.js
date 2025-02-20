69aadcc824719a11591fcd86e8dd0b4e8d638986
Sajjad Ahmad <jogimar14@gmail.com>
2025-02-21 04:18:48 +0500

    Style note cards and add delete functionlity

--- index.js
+++ index.js
@@ -2,22 +2,37 @@ document.addEventListener("DOMContentLoaded", () => {
   const addButton = document.querySelector(".add-btn");
 
   addButton.addEventListener("click", (e) => {
-    e.preventDefault(); // Prevents form submission
+    e.preventDefault();
     addNote();
   });
 
   function addNote() {
+    title = document.querySelector(".title").value;
+    content = document.querySelector(".note-text").value;
     const notesList = document.querySelector(".notes-list");
     const note = document.createElement("div");
     note.classList.add("note");
+    note.setAttribute("draggable", "true");
     note.innerHTML = `
-      <div class="tools">
-        <button class="edit"><i class="fas fa-edit"></i></button>
-        <button class="delete"><i class="fas fa-trash-alt"></i></button>
-      </div>
-      <div class="main hidden"></div>
-      <textarea></textarea>
-    `;
+    <h1>${title}</h1>
+    <p class = "content">${content}</p>
+      </p>
+    <div class="note-buttons">
+      <button class="edit">Edit</button>
+      <button class="delete">Delete</i></button>
+    </div>`;
     notesList.appendChild(note);
+  }
+
+  deleteButton = document.querySelector(".notes-list");
+  deleteButton.addEventListener("click", (e) => {
+    if (e.target.classList.contains("delete")) {
+      e.preventDefault();
+      deleteNote(e.target);
+    }
+  });
+
+  function deleteNote(btn) {
+    btn.parentElement.parentElement.remove();
   }
 });
